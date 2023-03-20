f08006daae6620eb3d0980962a2a5572908f1f34
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2023-03-20 20:41:15 +0100

    first commit

---
+++ src/screens/Home.tsx
@@ -0,0 +1,14 @@
+import { Text } from '@mantine/core'
+import React from 'react'
+
+interface HomeProps {}
+
+const Home: React.FC<HomeProps> = () => {
+  return (
+    <div>
+      {' '}
+      <Text>Welcome to Home!</Text>
+    </div>
+  )
+}
+export default Home
